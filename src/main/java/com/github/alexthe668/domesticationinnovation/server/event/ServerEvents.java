b69393408ba9861275719093ea500c4b6bccdf3d
Alex-the-666 <alex.rowlands@cox.net>
2022-05-19 10:52:58 -0700

    fix #44 fix peircing crash

--- src/main/java/com/github/alexthe668/domesticationinnovation/server/event/ServerEvents.java
+++ src/main/java/com/github/alexthe668/domesticationinnovation/server/event/ServerEvents.java
@@ -18,41 +18,7 @@ import java.util.UUID;
 @Mod.EventBusSubscriber(modid = DomesticationMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ServerEvents {
 
-    //list of pets to be teleported across dimensions, cleared after every tick
-    public static List<Triple<Entity, ServerLevel, UUID>> teleportingPets = new ArrayList<>();
 
-    @SubscribeEvent
-    public static void onServerTick(TickEvent.WorldTickEvent tick) {
-        if (!tick.world.isClientSide && tick.world instanceof ServerLevel) {
-            for (final var triple : teleportingPets) {
-                Entity entity = triple.a;
-                ServerLevel endpointWorld = triple.b;
-                UUID ownerUUID = triple.c;
-                entity.unRide();
-                entity.level = endpointWorld;
-                Entity player = endpointWorld.getPlayerByUUID(ownerUUID);
-                if (player != null) {
-                    Entity teleportedEntity = entity.getType().create(endpointWorld);
-                    if (teleportedEntity != null) {
-                        teleportedEntity.restoreFrom(entity);
-                        Vec3 toPos = player.position();
-                        EntityDimensions dimensions = entity.getDimensions(entity.getPose());
-                        AABB suffocationBox = new AABB(-dimensions.width / 2.0F, 0, -dimensions.width / 2.0F, dimensions.width / 2.0F, dimensions.height, dimensions.width / 2.0F);
-                        while(!endpointWorld.noCollision(entity, suffocationBox.move(toPos.x, toPos.y, toPos.z)) && toPos.y < 300){
-                            toPos = toPos.add(0, 1, 0);
-                        }
-                        teleportedEntity.moveTo(toPos.x, toPos.y, toPos.z, entity.getYRot(), entity.getXRot());
-                        teleportedEntity.setYHeadRot(entity.getYHeadRot());
-                        teleportedEntity.fallDistance = 0.0F;
-                        teleportedEntity.setPortalCooldown();
-                        endpointWorld.addFreshEntity(teleportedEntity);
-                    }
-                    entity.remove(Entity.RemovalReason.DISCARDED);
-                }
-            }
-            teleportingPets.clear();
-        }
-    }
 
 
 }
