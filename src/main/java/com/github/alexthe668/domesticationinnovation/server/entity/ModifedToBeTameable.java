22e33aff6a0d4e5d6289125c6c753d8c3e68d3b8
Alexthe668 <alex.rowlands@cox.net>
2022-10-31 15:40:46 -0700

    fix #83

--- src/main/java/com/github/alexthe668/domesticationinnovation/server/entity/ModifedToBeTameable.java
+++ src/main/java/com/github/alexthe668/domesticationinnovation/server/entity/ModifedToBeTameable.java
@@ -1,11 +1,13 @@
 package com.github.alexthe668.domesticationinnovation.server.entity;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.OwnableEntity;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
 
-public interface ModifedToBeTameable {
+public interface ModifedToBeTameable extends OwnableEntity {
     boolean isTame();
     void setTame(boolean value);
     @Nullable
@@ -18,4 +20,14 @@ public interface ModifedToBeTameable {
     boolean isFollowingOwner();
 
     boolean isValidAttackTarget(LivingEntity target);
+
+    @Nullable
+    default UUID getOwnerUUID(){
+        return getTameOwnerUUID();
+    }
+
+    @Nullable
+    default Entity getOwner(){
+        return getTameOwner();
+    }
 }
